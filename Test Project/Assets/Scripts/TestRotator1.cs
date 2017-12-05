04d42ef2b595d9783557c5622b4921685103c864
StanleyvanEssen <stanleyessen2001@github.com>
2017-12-05 12:48:30 +0100

    Gun model geadd
    Work in progress, tot nu toe is het nog 2d maar ik maak hem nog 3d

---
+++ Test Project/Assets/Scripts/TestRotator1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotator1 : MonoBehaviour
+{
+    public Vector3 v2;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        v2.x = -Input.GetAxis("Mouse Y");
+        transform.Rotate(v2);
+    }
+}
---
+++ Test Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        TestVoid(15);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void TestVoid(int i)
+    {
+        i += 2;
+        print(i);
+    }
+}
