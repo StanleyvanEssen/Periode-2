530432fd36e48d0fe83e67948ff075e7d4b258d7
StanleyvanEssen <stanleyessen2001@github.com>
2017-12-10 13:54:23 +0100

    Gun 3d gemaakt en meerdere verts verwijdert voor optimalisatie

---
+++ Test Project/Assets/Scripts/DamageModifier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageModifier : MonoBehaviour
+{
+    public float endDamage;
+    public float fireModifier;
+
+	// Use this for initialization
+	void Start ()
+    {
+        fireModifier = 1.06f;
+        endDamage = Damage(100);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public float Damage (float standardDamage)
+    {
+        return standardDamage * fireModifier;
+    }
+}
---
+++ Test Project/Assets/Scripts/ReturnTypes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnTypes : MonoBehaviour
+{
+    public float resultaat;
+
+	// Use this for initialization
+	void Start ()
+    {
+        resultaat = Btw(100);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public float Btw (float bruto)
+    {
+        return bruto * 1.21f;
+    }
+}
