4d782ff1b11bd2416c8a44d2d3cf5feb5f111bc4
StanleyvanEssen <stanleyessen2001@github.com>
2017-12-14 18:53:22 +0100

    Added prototype game, edited the gun model a little and started making main character

---
+++ Neon Prototype/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 v;
+    public float hor;
+    public float ver;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        v.x = hor;
+        v.z = ver;
+        transform.Translate(v * speed * Time.deltaTime);
+    }
+}
---
+++ Neon Prototype/Assets/Scripts/TestRotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotator : MonoBehaviour
+{
+    public Vector3 v;
+    public float hor;
+    public float ver;
+    public float speed;
+    public Vector3 v2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        v.x = hor;
+        v.y = ver;
+        transform.Translate(v * speed * Time.deltaTime);
+        v2.y = Input.GetAxis("Mouse X");
+        transform.Rotate(v2);
+    }
+}
