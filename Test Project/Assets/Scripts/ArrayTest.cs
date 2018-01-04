b14243e1598f6377f194a06c5da709df5ac241a2
StanleyvanEssen <stanleyessen2001@github.com>
2018-01-05 00:17:32 +0100

    made the luger (makarov, file is just names wrong, gonna fix that later) 3d

---
+++ Test Project/Assets/Scripts/ArrayTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour
+{
+    public List<int> il = new List<int>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        il.AddRange(Random.Range(10,13));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
