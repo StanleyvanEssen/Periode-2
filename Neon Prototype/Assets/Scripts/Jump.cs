d85d0b05c1461941d20d48129b529369b6517ef7
StanleyvanEssen <stanleyessen2001@github.com>
2018-01-08 10:55:00 +0100

    Added the unity folders to fix the library problem

---
+++ Neon Prototype/Assets/Scripts/Jump.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody r;
+    public Vector3 v;
+    public int mayJump;
+
+    // Use this for initialization
+    void Start ()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mayJump >= 1)
+        {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                r.velocity = v;
+                mayJump = mayJump - 1;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (gameObject)
+        {
+            mayJump = 2;
+        }
+    }
+}
