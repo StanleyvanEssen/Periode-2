ed4ff1fdc2168a30f5824294ec059132d07191a8
StanleyvanEssen <stanleyessen2001@github.com>
2018-01-14 14:02:38 +0100

    raycast gefixd, prototype groter gemaakt, makarov bijgewerkt, building gemaakt (bijna af) en straatlicht gemaakt (bijna klaar)

--- Neon Prototype/Assets/Scripts/RayCast.cs
+++ Neon Prototype/Assets/Scripts/RayCast.cs
@@ -16,14 +16,12 @@ public class RayCast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * 10, Color.magenta);
         if (Input.GetButtonDown("Fire1"))
         {
             if (Physics.Raycast(transform.position, transform.forward, out hit, 1000f))
             {
                 if (hit.transform.tag == "Sample Text")
                 {
-                    print("Xander is toxic");
                     GameObject G = Instantiate(particle, hit.point, Quaternion.identity);
                     Destroy(G, 1);
                     hit.transform.gameObject.GetComponent<Health>().LoseHP(1);
